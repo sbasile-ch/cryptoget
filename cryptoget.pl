67a50f8062911c192bf53ece782ab2c5c38fb755
sbasile-ch <sbasile@companieshouse.gov.uk>
2018-11-26 20:54:17 +0000

    formats json into table and improves input checks

--- cryptoget.pl
+++ cryptoget.pl
@@ -1,31 +1,66 @@
 #! /usr/bin/env perl
-
 use Getopt::Long;
 
+#__________________ OPTIONS
+sub opt_info {($type, $desc) = @_; return {'type'=>$type, 'desc'=>$desc}}
+sub opt_name {($opt) = @_; return  sprintf "--%s|-%s", $opt, substr $opt, 0, 1 }
 @CURRENCY_PAIRS = qw / btcusd btceur eurusd xrpusd xrpeur xrpbtc ltcusd ltceur
                        ltcbtc ethusd etheur ethbtc bchusd bcheur bchbtc /;
-$USAGE="Usage $0 --pair (" . join ('|', @CURRENCY_PAIRS) . ') [--base_url] [--max_connection]';
-$EXIT_MSG=$USAGE;
+%OPT=( 'pair'           => opt_info ('=s',  'specify a currency from this range ('.join ('|', @CURRENCY_PAIRS) .')'),
+       'base-url'       => opt_info ( ':s', 'specify an API base URL' ),
+       'max-connection' => opt_info ( ':s', 'specify timeout on HTTP request' ));
+%VARS = ();
+#__________________ USAGE
+(( $l = length $_) && ($max_opt < $l) && ($max_opt = $l)) foreach keys %OPT;
+$max_opt += length '[--|--]';
+$USAGE = "Usage: $0 ".join (' ', ( map {sprintf "\n%-*s%s",  $max_opt, opt_name($_), $OPT{$_}{desc}} keys %OPT));
 
-sub Exit {
-    die "$EXIT_MSG\n";
+sub Exit { print "@_\n"; exit }
+#__________________
+sub init_args {
+   @getop = map { "$_$OPT{$_}{type}" } keys %OPT;
+
+   Exit($USAGE) if ! GetOptions(\%VARS, @getop) or $VARS{help};
+   #_______ set defaults
+   $VARS{'base-url'} = $ENV{BASE_URL} || 'https://www.bitstamp.net/api/v2/ticker/' if ! $VARS{'base-url'};
+   $VARS{'max-connection'} = $ENV{MAX_CURL_TIME} || 10 if ! $VARS{'max-connection'};
+   %COLS=() if $VARS{colour};
+
+   Exit("$err\n$USAGE") if ($err = check_args());
+}
+#__________________
+sub check_args {
+    %range = map { $_ => 1 } @CURRENCY_PAIRS;
+    return 'missing currency selection' if ! $VARS{pair};
+    return "$VARS{pair} not in range" if ! $range{$VARS{pair}};
+    return 0;
+}
+#__________________
+sub get_json {
+    $cmd = "curl -s -m $VARS{'max-connection'} $VARS{'base-url'}@_";
+    return ($_ = qx{$cmd 2>&1}, $? >> 8);  # return both curl response and curl exit code
+}
+#__________________
+sub print_json {
+    my ($str, %json_data) = @_;
+    for (; $str =~ /"([^"]+)"\s*:\s*"([^"]+)"/; $json_data{$1}=$2, $str=$'){}
+
+    $title = 'Current price as of ' . scalar(localtime($json_data{timestamp}));
+    $out   = "$title";
+    $line  = '=' x length($title);
+    $out  .= "\n$line\n" . join ("\n", sort (map { $_ eq 'timestamp' ?
+             () : "\t$_\t$json_data{$_}" } keys %json_data));
+    print "$out\n";
+}
+#__________________
+sub main {
+   init_args();
+   ($str, $exit_code) = get_json ($VARS{pair});
+   if ( $exit_code or (substr ($str, 0, 1) ne '{' )) {
+       printf "error on API call: [%s][%.30s..]\n", $exit_code, $str;
+   } else {
+       print_json($str);
+   }
 }
 
-$currency_pair = '';
-$BASE_URL;
-$MAX_CURL_TIME;
-
-print $USAGE;
-GetOptions(
-              'pair=s' => \$currency_pair,
-              'base_url:s' => \$BASE_URL,
-              'max_connection:s' => \$MAX_CURL_TIME
-          );
-$BASE_URL=$ENV{BASE_URL} || "https://www.bitstamp.net/api/v2/ticker/" if ! $BASE_URL;
-$MAX_CURL_TIME=$ENV{MAX_CURL_TIME} || 10 if ! $MAX_CURL_TIME;
-
-
-$json = `curl -m $MAX_CURL_TIME $BASE_URL$currency_pair`;
-print "retrievig $BASE_URL$currency_pair: $json\n";
-
-
+main;
